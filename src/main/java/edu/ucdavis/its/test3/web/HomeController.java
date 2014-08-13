7faa0baf9a5d86dffc00a027f40ba277bdde4980
Taniya P <taniya23@ad3.ucdavis.edu>
2014-08-13 13:28:34 -0700

    working hello real world

---
+++ src/main/java/edu/ucdavis/its/test3/web/HomeController.java
@@ -0,0 +1,22 @@
+package edu.ucdavis.its.test3.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping(value = "/")
+public class HomeController {
+    @RequestMapping("/")
+    public String index(Model model,  Principal principal) {
+        return "index";
+    }
+
+    @RequestMapping(value="/slides")
+    public String slides() {
+        return "slides";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <link rel = "stylesheet" type="text/css" href="resources/css/design.css">
+</head>
+    <div class = "first" style="-moz-border-radius: 20px">
+
+
+    </div>
+<body>
+<h2>HELLO REAL WORLD</h2>
+</body>
+</html>
